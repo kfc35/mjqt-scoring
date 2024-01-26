8b80a2897aed55d2f0631fb65ec2512d579de706
Kevin CHen <chen.kevin.f@gmail.com>
2024-01-26 01:13:57 -0500

    figuring out how scoring logic will work

--- src/model/hand/hk/hand.ts
+++ src/model/hand/hk/hand.ts
@@ -1,7 +1,8 @@
 "use strict";
 
 import { assertTilesNotNullAndCorrectLength } from "common/tileUtils";
-import { DragonTileValue, FlowerTile, SuitedOrHonorTile, SuitedOrHonorTileValue, SuitedTileValue, Tile, TileType, WindTileValue, flowerTileTypes } from "model/tile/tile";
+import { DragonTileValue, FlowerTile, GentlemenTileValue, SeasonTileValue, SuitedOrHonorTile, SuitedOrHonorTileValue, SuitedTileValue, Tile, TileType, TileValue, WindTileValue, flowerTileTypes } from "model/tile/tile";
+import { WinningHand } from "model/hand/hk/winningHand";
 
 export const handMinLength = 14;
 export const handMaxNumFlowers = 8;
@@ -16,6 +17,8 @@ export const maxUniqueTilePerHand = 4;
  * It may or may not represent a winning hand. */
 export class Hand {
     private _sortedTiles: Tile[];
+    private _tileToQuantity: ReadonlyMap<TileType, Map<TileValue, number>>;
+    private _winningHands: WinningHand[];
 
     constructor(unsortedTiles: Tile[]) {
         assertTilesNotNullAndCorrectLength(unsortedTiles, handMinLength, handMaxLength);
@@ -29,22 +32,24 @@ export class Hand {
             index + 1 > flowerTiles.length ? hasDup : hasDup || tile.equals(flowerTiles[index+1]!), false)) {
             throw new TypeError("A Hand cannot have duplicate flower tiles.");
         }
-
         const nonFlowerTiles : SuitedOrHonorTile[] = sortedCopy.filter(tile => !flowerTileTypes.has(tile.getType())) as SuitedOrHonorTile[];
-        if (nonFlowerTiles.length < 14) {
-            throw new TypeError("A Hand must have at least 14 non flower type tiles.");
+        if (nonFlowerTiles.length < handMinLength) {
+            throw new TypeError("A Hand must have at least " + handMinLength + " non flower tiles.");
         }
-        const tileToQuantity: Map<TileType, Map<SuitedOrHonorTileValue, number>> = new Map([
+
+        const tileToQuantity : Map<TileType, Map<TileValue, number>>= new Map([
             [TileType.BAMBOO, new Map<SuitedTileValue, number>()],
             [TileType.CIRCLE, new Map<SuitedTileValue, number>()],
             [TileType.CHARACTER, new Map<SuitedTileValue, number>()]
         ]);
         tileToQuantity.set(TileType.DRAGON, new Map<DragonTileValue, number>());
         tileToQuantity.set(TileType.WIND, new Map<WindTileValue, number>());
-        nonFlowerTiles.forEach(tile => {
-            const quantityMap: Map<SuitedOrHonorTileValue, number> = tileToQuantity.get(tile.getType())!;
-            quantityMap.set(tile.value as SuitedOrHonorTileValue, 
-                !quantityMap.get(tile.value as SuitedOrHonorTileValue) ? 1 : quantityMap.get(tile.value as SuitedOrHonorTileValue)! + 1);
+        tileToQuantity.set(TileType.GENTLEMEN, new Map<GentlemenTileValue, number>());
+        tileToQuantity.set(TileType.SEASON, new Map<SeasonTileValue, number>());
+        sortedCopy.forEach(tile => {
+            const quantityMap: Map<TileValue, number> = tileToQuantity.get(tile.getType())!;
+            quantityMap.set(tile.value as TileValue, 
+                !quantityMap.get(tile.value as TileValue) ? 1 : quantityMap.get(tile.value as SuitedOrHonorTileValue)! + 1);
         });
         // TODO write this more clearly. Basically unwrapping values and trying to find quantities greather than 4.
         const quantityMaps = [...tileToQuantity.values()];
@@ -63,10 +68,30 @@ export class Hand {
         // it's possible for 3 consecutive pongs to also be 3 identical chi
         // need to also check 7 pairs, any bespoke hands.
         this._sortedTiles = sortedCopy;
+        this._tileToQuantity = tileToQuantity;
+        this._winningHands = [];
     }
 
-    get sortedTiles() : Tile[] {
+    get sortedTiles() {
         return this._sortedTiles;
+    }
+
+    get tileToQuantity() {
+        return this._tileToQuantity;
+    }
+
+    get winningHands() {
+        return this._winningHands;
+    }
+
+    // returning "this" allows for chaining multiple analyzers.
+    // an analyzer, upon 
+    analyzeHandForWinCondition(analyzer: (hand: Hand) => WinningHand | undefined) : this {
+        const winningHand = analyzer(this);
+        if (winningHand) {
+            this._winningHands.push(winningHand);
+        }
+        return this;
     }
 
     // several candidate hand objects? you try to see if hand can be converted into a candidate hand object.
--- src/model/hand/hk/winningHand.ts
+++ src/model/hand/hk/winningHand.ts
@@ -4,13 +4,13 @@ import { Hand } from "model/hand/hk/hand.js";
 import { Meld } from "model/meld/meld.js";
 import { Tile, FlowerTile } from "model/tile/tile.js";
 
-/** A WinningHand is a Hand that has been analyzed for a win condition. */
-export class WinningHand extends Hand {
+/** A WinningHand is a Hand that has been analyzed for a win condition i.e. processed into melds. */
+export class WinningHand {
     melds: Meld[];
     bonusTiles: FlowerTile[];
+    // map from faan to "explanation", could be a single meld, multiple melds, individual tiles, whole hand, actions unrelated to your hand
 
-    constructor(tiles: Tile[], melds: Meld[], bonusTiles: FlowerTile[]) {
-        super(tiles);
+    constructor(melds: Meld[], bonusTiles: FlowerTile[]) {
         // assert each tile is not null
         // must have at least 14 non bonus tiles.
         // each individual mahjong tile can only appear max 4 times.
@@ -20,6 +20,11 @@ export class WinningHand extends Hand {
         // need to also check 7 pairs, any bespoke hands.
         this.melds = melds!;
         this.bonusTiles = bonusTiles!;
+    }
+
+    analyzeWinningHandForFaan(analyzer: (winningHand: WinningHand) => void) : this {
+        analyzer(this);
+        return this;
     }
 
     // several candidate hand objects? you try to see if hand can be converted into a candidate hand object.

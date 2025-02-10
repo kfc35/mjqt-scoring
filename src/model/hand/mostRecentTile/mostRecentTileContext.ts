2cc87319fdee3b37f675cb9ed5fce6bb4d97ad6a
Kevin Chen <chen.kevin.f@gmail.com>
2025-02-10 16:20:57 -0500

    fix most recent tile context for special hands

--- src/model/hand/mostRecentTile/mostRecentTileContext.ts
+++ src/model/hand/mostRecentTile/mostRecentTileContext.ts
@@ -4,14 +4,21 @@ import Meld from "model/meld/meld";
 export class MostRecentTileContext {
     private _tile: SuitedOrHonorTile;
     // _userSpecifiedMeld can be undefined for special hands, even if self drawn is false
-    // one MAY define a most recent tile where _isSelfDrawn = false and the userSpecifiedMeld = undefined
-    // in this case, the eaten tile will be used to convert an existing unexposed meld to an exposed one.
+    // TODO maybe we need a separate flag for self drawn... for special hands. i.e. you get your last orphan via discard.
     private _userSpecifiedMeld: Meld | undefined;
-
+    private _isSelfDrawn: boolean;
+    
+    constructor(tile: SuitedOrHonorTile, isSelfDrawn: boolean);
+    constructor(tile: SuitedOrHonorTile, userSpecifiedMeld?: Meld);
     constructor(tile: SuitedOrHonorTile,
-        userSpecifiedMeld: Meld | undefined) {
+        isSelfDrawnOrUserSpecifiedMeld: boolean | Meld | undefined) {
             this._tile = tile;
-            this._userSpecifiedMeld = userSpecifiedMeld;
+            if (isSelfDrawnOrUserSpecifiedMeld instanceof Meld || isSelfDrawnOrUserSpecifiedMeld === undefined) {
+                this._userSpecifiedMeld = isSelfDrawnOrUserSpecifiedMeld;
+                this._isSelfDrawn = (!!this._userSpecifiedMeld ? !this._userSpecifiedMeld.exposed : false);
+            } else {
+                this._isSelfDrawn = isSelfDrawnOrUserSpecifiedMeld;
+            }
     }
 
     get tile() : SuitedOrHonorTile {
@@ -19,7 +26,7 @@ export class MostRecentTileContext {
     }
 
     get isSelfDrawn() : boolean {
-        return !this._userSpecifiedMeld || !this._userSpecifiedMeld.exposed;
+        return (!!this._userSpecifiedMeld ? !this._userSpecifiedMeld.exposed : this._isSelfDrawn);
     }
 
     get userSpecifiedMeld() : Meld | undefined {

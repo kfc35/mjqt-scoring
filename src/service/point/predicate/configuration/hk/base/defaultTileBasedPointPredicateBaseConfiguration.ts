58aaf5357d36b954f0c4cfe6e434fab0e0bec8f8
Kevin Chen <chen.kevin.f@gmail.com>
2025-02-21 16:06:43 -0500

    tile based base configs

---
+++ src/service/point/predicate/configuration/hk/base/defaultTileBasedPointPredicateBaseConfiguration.ts
@@ -0,0 +1,71 @@
+import { PointPredicateID } from "model/point/predicate/pointPredicateID";
+import PointPredicateBaseConfiguration, { MAX_POINTS } from "service/point/predicate/configuration/base/pointPredicateBaseConfiguration";
+
+export function createDefaultJewelDragonPointPredicateBaseConfigurationMap(): Map<PointPredicateID, PointPredicateBaseConfiguration> {
+    const map: Map<PointPredicateID, PointPredicateBaseConfiguration> = new Map();
+    /* enabled by default */
+    map.set(PointPredicateID.ALL_HONORS_AND_TERMINALS, defaultAllHonorsAndTerminalsPointPredicateBaseConfiguration);
+    map.set(PointPredicateID.ALL_ONE_SUIT_AND_HONORS, defaultAllOneSuitAndHonorsPointPredicateBaseConfiguration);
+    map.set(PointPredicateID.ALL_ONE_SUIT, defaultAllOneSuitPointPredicateBaseConfiguration);
+    map.set(PointPredicateID.ALL_HONORS, defaultAllHonorsPointPredicateBaseConfiguration);
+    map.set(PointPredicateID.ALL_TERMINALS, defaultAllTerminalsPointPredicateBaseConfiguration);
+
+    /* disabled by default */
+    map.set(PointPredicateID.ALL_SIMPLES, defaultAllSimplesPointPredicateBaseConfiguration);
+    map.set(PointPredicateID.VOIDED_SUIT, defaultVoidedSuitPointPredicateBaseConfiguration);
+    return map;
+}
+
+/** enabled by default */
+export const defaultAllHonorsAndTerminalsPointPredicateBaseConfiguration = 
+    new PointPredicateBaseConfiguration.Builder()
+        .enabled(true)
+        .points(1)
+        .isBonus(false)
+        // this is a point in addition to pongs/kongs
+        .build();
+
+export const defaultAllOneSuitAndHonorsPointPredicateBaseConfiguration = 
+    new PointPredicateBaseConfiguration.Builder()
+        .enabled(true)
+        .points(3)
+        .isBonus(false)
+        .build();
+
+
+export const defaultAllOneSuitPointPredicateBaseConfiguration = 
+    new PointPredicateBaseConfiguration.Builder()
+        .enabled(true)
+        .points(6)
+        .isBonus(false)
+        .build();
+
+export const defaultAllHonorsPointPredicateBaseConfiguration = 
+    new PointPredicateBaseConfiguration.Builder()
+        .enabled(true)
+        .points(MAX_POINTS)
+        .isBonus(false)
+        .build();
+
+
+export const defaultAllTerminalsPointPredicateBaseConfiguration = 
+    new PointPredicateBaseConfiguration.Builder()
+        .enabled(true)
+        .points(MAX_POINTS)
+        .isBonus(false)
+        .build();
+
+/** disabled by default */
+export const defaultAllSimplesPointPredicateBaseConfiguration = 
+    new PointPredicateBaseConfiguration.Builder()
+        .enabled(false)
+        .points(1)
+        .isBonus(false)
+        .build();
+
+export const defaultVoidedSuitPointPredicateBaseConfiguration = 
+    new PointPredicateBaseConfiguration.Builder()
+        .enabled(false)
+        .points(1)
+        .isBonus(false)
+        .build();

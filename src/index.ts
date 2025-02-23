600f5888fc8aa771c287c9f65ff734958f9961bd
Kevin Chen <chen.kevin.f@gmail.com>
2025-02-23 15:06:32 -0500

    add build steps, testing framework, fix bug

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+/** api */
+export * from "api/scorer";
+
+/** common */
+export * from "common/deck";
+export * from "common/meldUtils";
+export * from "common/tileUtils";
+
+/** model */
+export * from "model/hand/hk/hand";
+export * from "model/hand/hk/winningHand/meldBasedWinningHand";
+export * from "model/hand/hk/winningHand/specialWinningHand";
+export * from "model/hand/hk/winningHand/specialWinningHandType";
+export * from "model/hand/hk/winningHand/tileGroupValueMaps";
+export * from "model/hand/hk/winningHand/winningHand";
+export * from "model/hand/mostRecentTile/mostRecentTileContext";
+
+export * from "model/point/predicate/pointPredicateID";
+
+export * from "model/tile/tile";
+export * from "model/tile/tileGroup";
+export * from "model/tile/tileValue";
+export * from "model/tile/group/bambooTile";
+export * from "model/tile/group/characterTile";
+export * from "model/tile/group/circleTile";
+export * from "model/tile/group/dragonTile";
+export * from "model/tile/group/flowerTile";
+export * from "model/tile/group/gentlemanTile";
+export * from "model/tile/group/honorTile";
+export * from "model/tile/group/honorTileConstructor";
+export * from "model/tile/group/seasonTile";
+export * from "model/tile/group/suitedOrHonorTile";
+export * from "model/tile/group/suitedTile";
+export * from "model/tile/group/suitedTileConstructor";
+export * from "model/tile/group/tileFactory";
+export * from "model/tile/group/windTile";
+export * from "model/tile/hk/hongKongTile";
+export * from "model/tile/quantityMap/tileQuantityMap";
+
+export * from "model/roundContext/roundContext";
+export * from "model/roundContext/windDirection";
+
+export * from "model/winContext/winContext";
+
+/** service */
+export * from "service/handAnalyzer/hk/handAnalyzer";
+export * from "service/point/evaluator/pointEvaluation";
+export * from "service/point/evaluator/hk/pointEvaluator";
+export * from "service/point/evaluator/hk/pointEvaluator";
+
+export * from "service/point/predicate/configuration/root/rootPointPredicateConfiguration";
+export * from "service/point/predicate/configuration/base/pointPredicateBaseConfiguration";
+export * from "service/point/predicate/configuration/logic/pointPredicateLogicConfiguration";
+export * from "service/point/predicate/configuration/logic/pointPredicateLogicOption";
+export * from "service/point/predicate/configuration/hk/defaultPointPredicateBaseConfiguration";
+export * from "service/point/predicate/configuration/hk/defaultPointPredicateLogicConfiguration";
+export * from "service/point/predicate/configuration/hk/defaultRootPointPredicateConfiguration";
+
+export * from "service/point/predicate/result/pointPredicateResult";
+export * from "service/point/predicate/result/pointPredicateFailureResult";
+export * from "service/point/predicate/result/pointPredicateSingleSuccessResult";
+export * from "service/point/predicate/result/meldBased/pointPredicateFailureResultMeldDetail";
+export * from "service/point/predicate/result/meldBased/pointPredicateSuccessResultMeldDetail";
+export * from "service/point/predicate/result/tile/pointPredicateFailureResultTileDetail";
+export * from "service/point/predicate/result/tile/pointPredicateSuccessResultTileDetail";

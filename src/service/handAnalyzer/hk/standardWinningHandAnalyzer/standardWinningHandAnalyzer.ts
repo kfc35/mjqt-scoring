kfc35/mahjong-scoring

finish impl of analyzers

src/service/handAnalyzer/hk/standardWinningHandAnalyzer/standardWinningHandAnalyzer.ts
@@ -2,12 +2,18 @@ import { HandAnalyzer } from "service/handAnalyzer/hk/handAnalyzer";
 import { StandardWinningHand } from "model/hand/hk/standardWinningHand";
 import { Hand } from "model/hand/hk/hand";
 import { analyzeForSevenPairs } from "service/handAnalyzer/hk/standardWinningHandAnalyzer/sevenPairsAnalyzer";
+import { analyzeForFourNonPairMeldsAndOnePair } from "service/handAnalyzer/hk/standardWinningHandAnalyzer/fourNonPairMeldsAndOnePairAnalyzer";
 
 export const analyzeForStandardWinningHands : HandAnalyzer<StandardWinningHand> = (hand: Hand) => {
     // a standard winning hand, but pre-checked for simplicity of logic.
     const sevenPairsHand = analyzeForSevenPairs(hand);
     if (sevenPairsHand) {
         return sevenPairsHand;
+    }
+
+    const regularWinningHands = analyzeForFourNonPairMeldsAndOnePair(hand);
+    if (regularWinningHands) {
+        return regularWinningHands;
     }
 
     return [];

kfc35/mjqt-scoring

src/service/handAnalyzer/hk/standardWinningHandAnalyzer/standardWinningHandAnalyzer.ts
@@ -1,13 +1,19 @@
 import { HandAnalyzer } from "service/handAnalyzer/hk/handAnalyzer";
 import { StandardWinningHand } from "model/hand/hk/standardWinningHand";
 import { Hand } from "model/hand/hk/hand";
-import { analyzeForSevenPairs } from "service/handAnalyzer/hk/standardWinningHandAnalyzer/sevenPairsAnalyzer";
+import { analyzeForSevenPairs } from "service/handAnalyzer/base/standardWinningHandAnalyzer/sevenPairsAnalyzer";
+import { analyzeForFiveMeldsNoKnitted } from "service/handAnalyzer/base/standardWinningHandAnalyzer/fiveMeldsNoKnittedAnalyzer";
 
 export const analyzeForStandardWinningHands : HandAnalyzer<StandardWinningHand> = (hand: Hand) => {
     // a standard winning hand, but pre-checked for simplicity of logic.
     const sevenPairsHand = analyzeForSevenPairs(hand);
     if (sevenPairsHand) {
         return sevenPairsHand;
+    }
+
+    const regularWinningHands = analyzeForFiveMeldsNoKnitted(hand);
+    if (regularWinningHands) {
+        return regularWinningHands;
     }
 
     return [];